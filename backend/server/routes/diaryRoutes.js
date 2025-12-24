ef9a3fd70e4eea71f0947d554206fe674d821aa9
WoH3D <j200chi@naver.com>
2025-12-24 16:51:24 +0900

    코멘트 기능 구현

--- backend/server/routes/diaryRoutes.js
+++ backend/server/routes/diaryRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const diaryController = require('../../controllers/diaryController')
 
 // 일기 감정 분석
-router. post('/analyze', diaryController.analyzeDiary)
+router.post('/analyze', diaryController.analyzeDiary)
 
 // 주간 일기 조회
 router.get('/weekly', diaryController.getWeeklyDiary)
